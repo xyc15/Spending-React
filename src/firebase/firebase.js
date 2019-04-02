22a751c05f8ee2accf322414f6465d34b9cd62ef
Cathy <xiaoyingchangca@gmail.com>
2019-04-02 16:55:52 -0700

    add console to firebase.js file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -9,7 +9,7 @@ const config = {
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
   };
-
+console.log("config is: ", config.databaseURL)
 firebase.initializeApp(config);
 
 const database = firebase.database();
